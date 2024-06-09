2cda39468e99c56e5abdbb165ec29ea145269cc6
ioannis-tsagki <ioannistsagkaris@gmail.com>
2024-06-09 18:33:54 +0300

    .

--- HyperPace Gym/Assets/Scripts/MenuScripts/OptionsMenuScript.cs
+++ HyperPace Gym/Assets/Scripts/MenuScripts/OptionsMenuScript.cs
@@ -32,15 +32,6 @@ public class OptionsMenuScript : MonoBehaviour
         resolutionDropdown.AddOptions(options);
         resolutionDropdown.value = currentRsolutionIndex;
         resolutionDropdown.RefreshShownValue();
-
-        if (File.Exists(Application.persistentDataPath + "/Volume.txt") && File.ReadAllText(Application.persistentDataPath + "/Volume.txt") != null && File.ReadAllText(Application.persistentDataPath + "/Volume.txt") != "")
-            volumeSlider.value = float.Parse(File.ReadAllText(Application.persistentDataPath + "/Volume.txt"));
-        
-        if (File.Exists(Application.persistentDataPath + "/Sensitivity.txt") && File.ReadAllText(Application.persistentDataPath + "/Sensitivity.txt") != null && File.ReadAllText(Application.persistentDataPath + "/Sensitivity.txt") != "")
-            sensitivitySlider.value = float.Parse(File.ReadAllText(Application.persistentDataPath + "/Sensitivity.txt"));
-
-        if (File.Exists(Application.persistentDataPath + "/Fullscreen.txt") && File.ReadAllText(Application.persistentDataPath + "/Fullscreen.txt") != null && File.ReadAllText(Application.persistentDataPath + "/Fullscreen.txt") != "")
-            fullscreenToggle.isOn = bool.Parse(File.ReadAllText(Application.persistentDataPath + "/Fullscreen.txt"));
     }
 
     public void SetVolume(float volume) {
